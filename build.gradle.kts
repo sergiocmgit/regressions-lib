a6d218387749aa7ecfee11fed709cbddc15d3e2e
sergiocostamoreno <47816978+sergiocostamoreno@users.noreply.github.com>
2023-10-16 22:14:26 +0200

    gradle settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,45 +1,25 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-	id("org.springframework.boot") version "3.1.0"
-	id("io.spring.dependency-management") version "1.1.0"
-	kotlin("jvm") version "1.8.21"
-	kotlin("plugin.spring") version "1.8.21"
-	id("org.jetbrains.kotlinx.dataframe") version "0.10.0"
+	kotlin("jvm") version "1.9.10"
 }
 
-group = "practice.kotlin"
+group = "com.scmoreno.regression"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
 	mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlinx:multik-core:0.2.2")
 	implementation("org.jetbrains.kotlinx:multik-default:0.2.2")
 
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-    implementation(kotlin("stdlib-jdk8"))
+	testImplementation("org.springframework.boot:spring-boot-starter-test:3.1.4")
 }
 
-tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
-	}
+kotlin {
+	jvmToolchain(17)
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
-}
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
 }
